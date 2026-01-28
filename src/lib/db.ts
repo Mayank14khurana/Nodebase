aa5ca60c402ac6ccced66f39daf3bdff23fa547c
Mayank Khurana <mayankkhurana192@gmail.com>
2026-01-29 00:11:34 +0530

    trpc setup

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,18 +1,34 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
+  prisma: PrismaClient
 }
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+const url = new URL(process.env.DATABASE_URL!)
+
+const pool = new Pool({
+  host: '98.89.62.209',
+  port: 5432,
+  user: url.username,
+  password: url.password,
+  database: url.pathname.slice(1),
+  ssl: {
+    servername: url.hostname,
+    rejectUnauthorized: false,
+  }
 })
 
-const prisma = globalForPrisma.prisma || new PrismaClient({adapter});
+// Force recreate in dev to pick up changes
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = undefined;
+
+const adapter = new PrismaPg(pool);
+
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') {
-    globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma = prisma;
 }
 
 export default prisma;
